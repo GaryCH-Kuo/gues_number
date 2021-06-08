72c269378a1b034d6d4a41bb78b36791f32847f8
Gary Kuo <gary770503@gmail.com>
2021-06-08 16:18:23 +0800

    add the random number  and count time

--- guess_number.py
+++ guess_number.py
@@ -1,16 +1,20 @@
 import random
-r = random.randint(1, 100)
-x = 10
-while x > 0 :
-	x = x - 1
+start = input('請輸入隨機數字範圍開始值:')
+start = int(start)
+end = input('請輸入隨機數字範圍結束值:')
+end = int(end)
+
+r = random.randint(start, end)
+x = 0
+while True:
+	x = x + 1
 	num = input('請猜出終結數字:')
 	num = int(num)
 	if num == r:
-		print('你猜中了!!!')
+		print('恭喜你猜中了!!!第', x,'次猜中')
 		break
 	elif num > r:
 		print('比終結數字大')
 	elif num < r:
 		print('比終結數字小')
-		if x == 0:
-			print('幹，給你10次機會還猜不對!!!你是豬阿!!!')
+	print('已經猜了', x,'次!!!')
